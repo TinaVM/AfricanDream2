fdd9680ab5a13be7136e38362fcbf472a34e36a0
TinaVM <92442291+TinaVM@users.noreply.github.com>
2022-11-08 01:36:07 +0200

    commit

--- adminUsers.php
+++ adminUsers.php
@@ -23,7 +23,35 @@
                     <th>Password</th>
                 </tr>
             </thead>
-            
+            <tbody>
+                <?php 
+                $servername = "localhost";
+                $username = "root";
+                $password = "";
+                $database = "african";
+
+
+                //creating connection
+                $connection = new mysqli($servername,$username,$password,$database);
+
+                //check connection
+                if($connection->connect_error){
+                    die("Connection failed: " . $connection->connect_error);
+                }
+                ?>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td>
+                        <a class='btn btn-primary btn sm' href='/AfricanDream/editUser.php'>Edit</a>
+                        <a class='btn btn-danger btn sm' href='/AfricanDream/deleteUser.php'>Edit</a>
+                    </td>
+                </tr>
+            </tbody>
         </table>
     </div>
 </body>
