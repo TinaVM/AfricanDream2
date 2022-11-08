d5ebf24f47f4c87261cc500090f3587300e4a4bc
TinaVM <92442291+TinaVM@users.noreply.github.com>
2022-11-08 03:06:13 +0200

    commit

--- createUser.php
+++ createUser.php
@@ -42,6 +42,7 @@ $successMessage = "User added correctly";
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Create User</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js"></script>
 </head>
 <body>
     <div class="container my-5">
@@ -88,6 +89,23 @@ $successMessage = "User added correctly";
                     <input type="text" class="form-control" name="password" value="<?php echo $password; ?>">
                 </div>
             </div>
+
+            <?php 
+            if(!empty($successMessage)){
+                echo"
+                <div class='row mb-3'>
+                <div class='offset-sm-3 col-sm-6'>
+                <div class='alert alert-success alert-dismissible fade show' role='alert'>
+                    <strong>$successMessage</strong>
+                    <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
+                </div>
+                </div>
+                </div>    
+                ";
+                
+            }
+            ?>
+
             <div class="row mb-3">
                 <div  class="offset-sm-3 col-sm-3 d-grid">
                     <button type="submit" class="btn btn-primary">Submit</button>
